dd11866db30d8099ff0f4b26d5367509b8b6e917
Cools9 <rishabh4a@christacademy.in>
2026-06-19 14:49:49 +0530

    added tiny apples(not functional)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use germterm::{
-    color::Color, crossterm::event::{Event, KeyCode, KeyEvent}, draw::{draw_fps_counter, draw_octad, draw_rect, draw_text, draw_twoxel}, engine::{Engine, end_frame, exit_cleanup, init, start_frame}, input::poll_input, layer::create_layer
+    color::Color, crossterm::event::{Event, KeyCode, KeyEvent}, draw::{self, draw_fps_counter, draw_octad, draw_rect, draw_text, draw_twoxel}, engine::{Engine, end_frame, exit_cleanup, init, start_frame}, input::poll_input, layer::create_layer
 };
 use std::io;
-
+use rand::prelude::*;
 struct Snake{
     x:f32,
     y:f32
@@ -20,7 +20,10 @@ fn main() -> io::Result<()> {
         x:3.0,
         y:4.0
     };
-    let mut engine = Engine::new(100, 40)
+    let TERM_COLS:u16=100;
+    let TERM_ROWS:u16=40;
+
+    let mut engine = Engine::new(TERM_COLS, TERM_ROWS)
         .limit_fps(30);
     let layer = create_layer(&mut engine, 0);
 
@@ -47,13 +50,19 @@ fn main() -> io::Result<()> {
             }
         }
         match direction{
-            Direction::UP => snake.y += 1.0,
-            Direction::DOWN => snake.y -= 1.0,
-            Direction::LEFT => snake.x -= 1.0,
-            Direction::RIGHT => snake.x += 1.0,
+            Direction::UP => snake.y -= 0.5,
+            Direction::DOWN => snake.y += 0.5,
+            Direction::LEFT => snake.x -= 0.5,
+            Direction::RIGHT => snake.x += 0.5,
         }
         draw_twoxel(&mut engine, layer, snake.x, snake.y, Color::RED);
         draw_fps_counter(&mut engine, layer, 0, 0);
+        let mut rng = rand::rng();
+        for i in 1..5{
+            let x_range:f32=rng.random_range(0..TERM_COLS).into();
+            let y_range:f32=rng.random_range(0..TERM_ROWS).into();
+            draw_twoxel(&mut engine, layer, x_range,y_range, Color::BLUE);
+        }
 
         // End the frame
         end_frame(&mut engine)?;
